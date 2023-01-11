d271a7118fa73294567ff1e231e2f21a7f06d10a
Jeff-the-simp <118701318+Jeff-the-simp@users.noreply.github.com>
2023-01-11 10:59:22 +0800

    recreated folder and added variable-story project

---
+++ variable-story/lesson1.js
@@ -0,0 +1,31 @@
+const hi = 3.14
+let why = 5
+
+const reader = 'whoever is reading this,'
+const student1 = 'Jeff'
+const teacher1 = 'Mr.Kung'
+const ruler1 = 'Pamela'
+const student2 = 'Markus'
+
+document.write(" Hi "+ reader,)
+
+
+document.write(' few years ago '+ student1 +" met "+ student2+' at PAS. ')
+
+document.write(`${ student1 } secretly loved ${ student2 }, `)
+
+
+document.write('however, '+ruler1+ ' did not allow students to have relationship and gossip at school, ')
+
+document.write('so '+student1+' asked '+teacher1+ ' to negociate with her.')
+
+document.write(' Unfortunately '+teacher1+ ' was fired by her after telling her the request. ')
+
+document.write( student1 + ' heard this news then called ' + student2 + ' to jump down the building as an apology for ' + teacher1)
+
+
+const sample1 = prompt('Are you part of the PAS community?')
+const sample2 = prompt('What is your name?')
+const reader1 = prompt('who are you')
+
+document.write("<img src='https://pacificamerican.org/wp-content/uploads/2020/04/MG_0925.jpg'/>")
